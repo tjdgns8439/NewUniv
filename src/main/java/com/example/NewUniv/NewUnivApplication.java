a4b07d5bcbc99334f251935c4281b9e8e5060cc7
seonghun <tjdgns8439@naver.com>
2023-02-13 17:05:15 +0900

    first commit

---
+++ src/main/java/com/example/NewUniv/NewUnivApplication.java
@@ -0,0 +1,13 @@
+package com.example.NewUniv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewUnivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewUnivApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NewUniv/NewUnivApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NewUniv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewUnivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
