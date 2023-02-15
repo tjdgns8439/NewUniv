8d4a6657d5b969a6ff5898d5f9574bc593904c85
seonghun <tjdgns8439@naver.com>
2023-02-15 19:04:02 +0900

    Create MemberRepository.java

---
+++ src/main/java/com/example/NewUniv/repository/MemberRepository.java
@@ -0,0 +1,23 @@
+package com.example.NewUniv.repository;
+
+import com.example.NewUniv.member.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    //리퍼지토리에 저장
+    Member save(Member member);
+
+    //리퍼지토리 내부에 있는 멤버 조회
+    Optional<Member> findById(Long Id);
+
+    //삭제
+    void delete(Member member);
+
+    //전체 조회
+    List<Member> findAll();
+
+    void clear();
+}
